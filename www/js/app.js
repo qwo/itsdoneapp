ce63b63c497fcbb0fe2bea7f7c919989a6afdeab
stanley zheng <dev.stanzheng@gmail.com>
2014-11-07 17:28:21 -0500

    preliminary sign up and login page

--- www/js/app.js
+++ www/js/app.js
@@ -58,6 +58,11 @@ angular.module('starter', ['ionic', 'starter.controllers','chat.controllers'])
       templateUrl: 'templates/service.html',
       controller: 'ChatCtrl'
     })
+    .state('signup', {
+      url: '/signup',
+      templateUrl: 'templates/signup.html',
+      controller: 'TestCtrl'
+    })
     .state('listings', {
       url: '/listings',
       templateUrl: 'templates/listings.html',
@@ -65,5 +70,5 @@ angular.module('starter', ['ionic', 'starter.controllers','chat.controllers'])
     });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/listings');
+  $urlRouterProvider.otherwise('/login');
 });
