c315dc819098da3e916f873e9966ed2e8214e494
stanley zheng <dev.stanzheng@gmail.com>
2014-11-09 00:28:26 -0500

    adding assets for payment page

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers','chat.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers','chat.controllers', 'angularPayments'])
 
 //This filter will give us the index of the item in the array or null if not found.
 .filter('messageByExpires', function() {
